14b78581265eb91f56c7921df514056df2ef3ade
matternstsr <matternstsr@gmail.com>
2024-10-29 17:14:04 -0500

    working on 1

--- red_black_tree/rb_trees.h
+++ red_black_tree/rb_trees.h
@@ -42,9 +42,4 @@ typedef struct rb_tree_s
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color);
 int rb_tree_is_valid(const rb_tree_t *tree);
 
-/* Helper functions */
-static int count_black_height(const rb_tree_t *tree);
-static int check_colors(const rb_tree_t *tree);
-static int check_bst(const rb_tree_t *tree, int min, int max);
-
 #endif /* RB_TREES_H */
