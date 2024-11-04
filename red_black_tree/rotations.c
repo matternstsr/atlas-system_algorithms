1461cc1c83d12df0eff63ad50e7d4ddf76613946
matternstsr <matternstsr@gmail.com>
2024-11-04 17:29:14 -0600

    working on 2 again - still have a double free

--- red_black_tree/rotations.c
+++ red_black_tree/rotations.c
@@ -9,30 +9,30 @@
 */
 rb_tree_t *right_rotate(rb_tree_t **root, rb_tree_t *y)
 {
-	rb_tree_t *x = y->left;
+    rb_tree_t *x = y->left;
 
-	y->left = x->right;
-	if (x->right != NULL)
-		x->right->parent = y;
+    y->left = x->right;
+    if (x->right != NULL)
+        x->right->parent = y;
 
-	x->parent = y->parent;
-	if (y->parent == NULL)
-	{
-		*root = x;
-	}
-	else if (y == y->parent->right)
-	{
-		y->parent->right = x;
-	}
-	else
-	{
-		y->parent->left = x;
-	}
+    x->parent = y->parent;
+    if (y->parent == NULL)
+    {
+        *root = x;
+    }
+    else if (y == y->parent->right)
+    {
+        y->parent->right = x;
+    }
+    else
+    {
+        y->parent->left = x;
+    }
 
-	x->right = y;
-	y->parent = x;
+    x->right = y;
+    y->parent = x;
 
-	return *root;
+    return *root;
 }
 
 /**
@@ -44,28 +44,28 @@ rb_tree_t *right_rotate(rb_tree_t **root, rb_tree_t *y)
 */
 rb_tree_t *left_rotate(rb_tree_t **root, rb_tree_t *x)
 {
-	rb_tree_t *y = x->right;
+    rb_tree_t *y = x->right;
 
-	x->right = y->left;
-	if (y->left != NULL)
-		y->left->parent = x;
+    x->right = y->left;
+    if (y->left != NULL)
+        y->left->parent = x;
 
-	y->parent = x->parent;
-	if (x->parent == NULL)
-	{
-		*root = y;
-	}
-	else if (x == x->parent->left)
-	{
-		x->parent->left = y;
-	}
-	else
-	{
-		x->parent->right = y;
-	}
+    y->parent = x->parent;
+    if (x->parent == NULL)
+    {
+        *root = y;
+    }
+    else if (x == x->parent->left)
+    {
+        x->parent->left = y;
+    }
+    else
+    {
+        x->parent->right = y;
+    }
 
-	y->left = x;
-	x->parent = y;
+    y->left = x;
+    x->parent = y;
 
-	return *root;
+    return *root;
 }
