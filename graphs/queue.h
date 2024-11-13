97baee5c2cd9e65e2eedb2553cb7916d2066093e
matternstsr <matternstsr@gmail.com>
2024-11-13 16:23:04 -0600

    hmm

--- graphs/queue.h
+++ graphs/queue.h
@@ -1,25 +1,34 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-#include "graphs.h"  
+#include <stddef.h>  /* For size_t */
+#include <stdbool.h> /* For bool type */
+#include <stdlib.h>  /* For malloc, free */
 
-/* queue structure */
-typedef struct queue_s
-{
-    vertex_t **items;   /* Array of ptrs vertex_t (queue will hold vertices) */
+/**
+ * struct queue_s - A simple queue data structure.
+ * 
+ * @size: The number of items currently in the queue.
+ * @capacity: The maximum number of items the queue can hold.
+ * @items: An array of items in the queue, representing the queue's data.
+ */
+typedef struct queue_s {
+    size_t size;        /* The number of items currently in the queue */
     size_t capacity;    /* The maximum number of items the queue can hold */
-    size_t front;       /* The index of the front of the queue */
-    size_t rear;        /* The index of the rear of the queue */
-    size_t size;        /* Current number of elements in the queue */
+    void **items;       /* Pointer to the array of items (void pointers for flexibility) */
+    size_t front;       /* The front index of the queue */
+    size_t rear;        /* The rear index of the queue */
 } queue_t;
 
-/* Function declarations for queue operations */
+/* Macro to get the size of the queue (number of elements in the queue) */
+#define QUEUE_SIZE(q) ((q)->size)
+
+/* Function prototypes for queue operations */
 queue_t *queue_create(size_t capacity);
 void queue_delete(queue_t *queue);
-int queue_enqueue(queue_t *queue, vertex_t *vertex);
-vertex_t *queue_dequeue(queue_t *queue);
-int queue_is_empty(queue_t *queue);
-int queue_is_full(queue_t *queue);
-size_t queue_size(queue_t *queue);
+bool queue_is_empty(queue_t *queue);
+bool queue_is_full(queue_t *queue);
+void queue_enqueue(queue_t *queue, void *item);
+void *queue_dequeue(queue_t *queue);
 
 #endif /* QUEUE_H */
