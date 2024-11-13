15bf483ef384f94d54d17c4222453f9c73f91b36
matternstsr <matternstsr@gmail.com>
2024-11-13 15:55:47 -0600

    going back that didnt help

---
+++ graphs/3-graph_delete.c
@@ -0,0 +1,29 @@
+#include "graphs.h"
+
+/**
+* graph_delete - Deletes all vertices and edges in the graph and frees the associated memory.
+* @graph: PTR to the graph structure to be deleted.
+*
+* Return: void (No return value).
+*/
+
+void graph_delete(graph_t *graph) {
+    if (!graph)
+        return;
+
+    vertex_t *vertex = graph->vertices;
+    while (vertex) {
+        edge_t *edge = vertex->edges;
+        while (edge) {
+            edge_t *temp = edge;
+            edge = edge->next;
+            free(temp);
+        }
+        free(vertex->content);
+        vertex_t *temp = vertex;
+        vertex = vertex->next;
+        free(temp);
+    }
+
+    free(graph);
+}
