337816e2f0684e7fb7062fd4427d7d26abe28e99
matternstsr <matternstsr@gmail.com>
2024-11-24 21:59:09 -0600

    sending what i have so far

--- huffman_coding/heap/binary_tree_node.c
+++ huffman_coding/heap/binary_tree_node.c
@@ -10,16 +10,16 @@
 */
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data)
 {
-	binary_tree_node_t *node;
+    binary_tree_node_t *node;
 
-	node = malloc(sizeof(binary_tree_node_t));
-	if (!node)
-		return (NULL);
+    node = malloc(sizeof(binary_tree_node_t));
+    if (!node)
+        return (NULL);
 
-	node->data = data;
-	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
+    node->data = data;
+    node->parent = parent;
+    node->left = NULL;
+    node->right = NULL;
 
-	return (node);
+    return (node);
 }
--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -9,15 +9,15 @@
 */
 heap_t *heap_create(int (*data_cmp)(void *, void *))
 {
-	heap_t *heap;
+    heap_t *heap;
 
-	heap = malloc(sizeof(heap_t));
-	if (!heap)
-		return (NULL);
+    heap = malloc(sizeof(heap_t));
+    if (!heap)
+        return (NULL);
 
-	heap->size = 0;
-	heap->root = NULL;
-	heap->data_cmp = data_cmp;
+    heap->size = 0;
+    heap->root = NULL;
+    heap->data_cmp = data_cmp;
 
-	return (heap);
+    return (heap);
 }
