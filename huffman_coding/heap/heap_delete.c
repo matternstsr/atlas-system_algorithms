1759f776207eb1e357665b45f01b45f4effd11bc
matternstsr <matternstsr@gmail.com>
2024-11-26 08:55:21 -0600

    putting delete back after fixing the others

--- huffman_coding/heap/heap_delete.c
+++ huffman_coding/heap/heap_delete.c
@@ -10,7 +10,14 @@ void heap_delete(heap_t *heap, void (*free_data)(void *))
 {
     if (!heap)
         return;
-
-    (void)free_data;
+    for (size_t i = 0; i < heap->size; i++)
+    {
+        if (heap->array[i])
+        {
+            free_data(heap->array[i]->data);
+            free(heap->array[i]);
+        }
+    }
+    free(heap->array);
     free(heap);
 }
