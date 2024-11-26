08d8159afdb0d8e5b670bfa2e61ea1ca25d467f7
matternstsr <matternstsr@gmail.com>
2024-11-26 10:07:28 -0600

    betty issues still

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -15,4 +15,11 @@ typedef struct symbol_s
 	size_t freq;
 } symbol_t;
 
+symbol_t *symbol_create(char data, size_t freq);
+binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size);
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
+int huffman_extract_and_insert(heap_t *priority_queue);
+static void generate_huffman_codes(
+	binary_tree_node_t *node,
+	char *code, size_t depth);
 #endif /* HUFFMAN_H */
