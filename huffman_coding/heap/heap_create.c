a4307a5075ff78224871f189e3c13de05d12181c
matternstsr <matternstsr@gmail.com>
2024-11-26 09:08:20 -0600

    putting delete back after fixing the others

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -11,11 +11,13 @@ heap_t *heap_create(int (*data_cmp)(void *, void *))
 {
 	heap_t *heap;
 
-	heap = calloc(1, sizeof(heap_t));
+	heap = malloc(sizeof(heap_t));
 	if (!heap)
 		return (NULL);
+
 	heap->size = 0;
 	heap->root = NULL;
 	heap->data_cmp = data_cmp;
+
 	return (heap);
 }
