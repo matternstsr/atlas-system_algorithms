a5f64cb633f067d3becfda8311e980171c69d2b6
matternstsr <matternstsr@gmail.com>
2024-11-25 21:01:50 -0600

    sending what i have so far

--- huffman_coding/heap/heap_insert.c
+++ huffman_coding/heap/heap_insert.c
@@ -2,6 +2,57 @@
 #include "heap.h"
 
 /**
+* insert_node - Inserts a new node into the heap at the correct position
+* @heap: Pointer to the heap to insert the node in
+* @data: Data to be stored in the new node
+*
+* Return: Pointer to the new node, or NULL if it fails
+*/
+binary_tree_node_t *insert_node(heap_t *heap, void *data)
+{
+	binary_tree_node_t *node;
+	binary_tree_node_t *current;
+	binary_tree_node_t *queue[1024];
+	int front = 0, rear = 0;
+
+	if (!heap || !data)
+		return (NULL);
+
+	node = binary_tree_node(NULL, data);
+	if (!node)
+		return (NULL);
+
+	if (heap->root == NULL)
+		heap->root = node;
+	else
+	{
+		current = heap->root, queue[rear++] = current;
+
+		while (front < rear)
+		{
+			current = queue[front++];
+
+			if (current->left == NULL)
+			{
+				current->left = node, node->parent = current;
+				break;
+			}
+			else
+				queue[rear++] = current->left;
+
+			if (current->right == NULL)
+			{
+				current->right = node, node->parent = current;
+				break;
+			}
+			else
+				queue[rear++] = current->right;
+		}
+	}
+	return (node);
+}
+
+/**
 * heap_insert - Inserts a value in a Min Binary Heap
 * @heap: Pointer to the heap to insert the node in
 * @data: Data to be stored in the new node
@@ -15,50 +66,11 @@ binary_tree_node_t *heap_insert(heap_t *heap, void *data)
 	if (!heap || !data)
 		return (NULL);
 
-	node = binary_tree_node(NULL, data);
+	node = insert_node(heap, data);
 	if (!node)
 		return (NULL);
 
-	if (heap->root == NULL)
-	{
-		heap->root = node;
-	}
-	else
-	{
-		binary_tree_node_t *current = heap->root;
-		binary_tree_node_t *queue[1024];
-		int front = 0, rear = 0;
-		queue[rear++] = current;
-
-		while (front < rear)
-		{
-			current = queue[front++];
-			if (current->left == NULL)
-			{
-				current->left = node;
-				node->parent = current;
-				break;
-			}
-			else
-			{
-				queue[rear++] = current->left;
-			}
-
-			if (current->right == NULL)
-			{
-				current->right = node;
-				node->parent = current;
-				break;
-			}
-			else
-			{
-				queue[rear++] = current->right;
-			}
-		}
-	}
-
 	bubble_up(node, heap);
-
 	heap->size++;
 	return (node);
 }
