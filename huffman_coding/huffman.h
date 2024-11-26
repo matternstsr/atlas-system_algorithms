b6c79c46ce2df5896096285e33ea995cd837769d
matternstsr <matternstsr@gmail.com>
2024-11-26 10:33:41 -0600

    betty issues still

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -18,13 +18,14 @@ typedef struct symbol_s
 	size_t freq;
 } symbol_t;
 
-
 symbol_t *symbol_create(char data, size_t freq);
-binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size);
+binary_tree_node_t *binary_tree_node_create(void *data);
 heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 int huffman_extract_and_insert(heap_t *priority_queue);
 void generate_huffman_codes(
 	binary_tree_node_t *node,
 	char *code, size_t depth);
+void heap_destroy(heap_t *heap);
 
 #endif /* HUFFMAN_H */
+
--- huffman_coding/symbol.c
+++ huffman_coding/symbol.c
@@ -2,12 +2,12 @@
 #include "huffman.h"
 
 /**
- * symbol_create - Creates a new symbol structure.
- * @data: The character to store in the symbol structure.
- * @freq: The frequency associated with the character.
- * Return: A pointer to the newly created symbol structure, or NULL if
- *         memory allocation fails.
- */
+* symbol_create - Creates a new symbol structure.
+* @data: The character to store in the symbol structure.
+* @freq: The frequency associated with the character.
+* Return: A pointer to the newly created symbol structure, or NULL if
+*         memory allocation fails.
+*/
 symbol_t *symbol_create(char data, size_t freq)
 {
 	symbol_t *symbol = malloc(sizeof(symbol_t));
