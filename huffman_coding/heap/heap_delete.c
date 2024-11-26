f4456b877861567131b4e0d97fc1533110f14042
matternstsr <matternstsr@gmail.com>
2024-11-26 10:00:27 -0600

    putting delete back after fixing the others

--- huffman_coding/heap/heap_delete.c
+++ huffman_coding/heap/heap_delete.c
@@ -11,6 +11,7 @@ void delete_node(binary_tree_node_t *node, void (*free_data)(void *))
 		return;
 	delete_node(node->left, free_data);
 	delete_node(node->right, free_data);
+	/* had if (node->data)*/
 	if (free_data)
 		free_data(node->data);
 	free(node);
