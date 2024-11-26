9a326b8aca4cb3efa48fd81563b6dd7aed924541
matternstsr <matternstsr@gmail.com>
2024-11-26 11:37:59 -0600

    frequency and then by character

--- huffman_coding/huffman_tree.c
+++ huffman_coding/huffman_tree.c
@@ -18,10 +18,10 @@ binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
 	if (!priority_queue)
 		return (NULL);
 
-	priority_queue = huffman_priority_queue(data, freq, size);
-	if (!priority_queue)
+	priority_heap = huffman_priority_queue(data, freq, size);
+	if (!priority_heap)
 		return (NULL);
-	while (prio_que->size > 1)
+	while (	if (!priority_heap)->size > 1)
 	{
 		if (!huffman_extract_and_insert(priority_queue))
 		{
