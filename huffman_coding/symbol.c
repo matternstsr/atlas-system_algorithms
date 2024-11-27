ade320fcf98a3a801942593c716cf7f39186c5b6
matternstsr <matternstsr@gmail.com>
2024-11-26 21:14:52 -0600

    betty again

---
+++ huffman_coding/symbol.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "huffman.h"
+
+/**
+* symbol_create - Creates a new symbol structure.
+* @data: The character to store in the symbol structure.
+* @freq: The frequency associated with the character.
+* Return: A pointer to the newly created symbol structure, or NULL if
+*         memory allocation fails.
+*/
+symbol_t *symbol_create(char data, size_t freq)
+{
+	symbol_t *symbol = malloc(sizeof(symbol_t));
+
+	if (!symbol)
+		return (NULL);
+
+	symbol->data = data;
+	symbol->freq = freq;
+
+	return (symbol);
+}
