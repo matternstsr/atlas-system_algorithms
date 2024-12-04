0b82adbe7eeeeb7649fa341b4176393b7a3cd102
matternstsr <matternstsr@gmail.com>
2024-12-04 11:07:41 -0600

    starting over this lesson stinks

--- pathfinding/pathfinding.h
+++ pathfinding/pathfinding.h
@@ -1,8 +1,6 @@
 #ifndef PATHFINDING_H
 #define PATHFINDING_H
 
-#include "queue.h"  // Now it correctly includes queue.h after point_t is defined
-
 /**
  * struct point_s - Structure storing coordinates
  *
