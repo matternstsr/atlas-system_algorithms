1e875b75092411a37bf3aea5ce2d0619723d5ace
matternstsr <matternstsr@gmail.com>
2024-12-05 10:13:01 -0600

    think i have it

--- pathfinding/pathfinding.h
+++ pathfinding/pathfinding.h
@@ -21,63 +21,6 @@ typedef struct point_s
 } point_t;
 
 /**
-* struct vertex_s - Structure storing a vertex in the graph
-* @name: Name of the vertex
-* @edges: List of edges connected to this vertex
-* @num_edges: List of edges connected to this vertex
-*/
-/* typedef struct vertex_s
-{
-	char *name;
-	struct edge_s *edges;
-	size_t num_edges;
-} vertex_t; */
-
-/**
-* struct edge_s - Structure for an edge in the graph
-* @vertex: The vertex the edge connects to
-* @weight: The weight of the edge (distance or cost)
-*/
-/* typedef struct edge_s
-{
-	vertex_t *vertex;
-	int weight;
-} edge_t; */
-
-/**
-* struct graph_s - Structure representing the graph
-* @vertices: List of vertices in the graph
-* @num_vertices: Number of vertices in the graph
-*/
-/* typedef struct graph_s
-{
-	vertex_t **vertices;
-	size_t num_vertices;
-} graph_t;
- */
-/**
-* struct queue_node - Node in the queue structure
-* @data: Data stored in the node
-* @next: Pointer to the next node
-*/
-/* typedef struct queue_node
-{
-	void *data;
-	struct queue_node *next;
-} queue_node_t; */
-
-/**
-* struct queue_s - Structure for a queue
-* @front: Pointer to the front of the queue
-* @rear: Pointer to the rear of the queue
-*/
-/* typedef struct queue_s
-{
-	queue_node_t *front;
-	queue_node_t *rear;
-} queue_t; */
-
-/**
 * backtracking_array - Searches for the first path from a starting point to
 *                      a target point within a 2D array using backtracking.
 * @map: The map (2D array) representing the grid
@@ -112,24 +55,5 @@ queue_t *backtracking_graph(graph_t *graph,
 */
 queue_t *dijkstra_graph(graph_t *graph,
 						vertex_t const *start, vertex_t const *target);
-
-/**
-* create_queue - Creates and initializes an empty queue.
-* Return: A ptr to newly created queue, or NULL if memory allocation fails.
-*/
-/* queue_t *create_queue(void);  */  /* Declare create_queue() */
-
-/**
-* enqueue - Adds a new element to the rear of the queue.
-* @queue: The queue to which the data will be added.
-* @data: A pointer to the data to be added to the queue.
-*/
-/* void enqueue(queue_t *queue, void *data);  */ /* Declare enqueue() */
-/**
-* dequeue - Removes and returns the element from the front of the queue.
-* @queue: The queue from which the data will be removed.
-* Return: A ptr to data removed from front of queue or NULL if queue is empty.
-*/
-/* void *dequeue(queue_t *queue);  Declare dequeue() */
 
 #endif /* PATHFINDING_H */
