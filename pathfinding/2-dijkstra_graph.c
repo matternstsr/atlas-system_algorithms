6ee26cbab72a93d70148fc4eaa87a9f9d40cfffe
matternstsr <matternstsr@gmail.com>
2024-12-05 23:04:41 -0600

    betty

--- pathfinding/2-dijkstra_graph.c
+++ pathfinding/2-dijkstra_graph.c
@@ -10,46 +10,46 @@
 queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
 						vertex_t const *target)
 {
-	queue_t *new_que = NULL;
-	int *this_visit, *this_dist;
+	queue_t *Nque = NULL;
+	int *currvis, *currdist;
 	vertex_t **prev, *vert = NULL;
 	size_t i = 0;
 
 	if (!graph || !start || !target)
 		return (NULL);
 	/* Allocate memory for queue and tracking arrays */
-	new_que = calloc(1, sizeof(queue_t));
-	if (!new_que)
+	Nque = calloc(1, sizeof(queue_t));
+	if (!Nque)
 		return (NULL);
-	this_visit = calloc(graph->nb_vertices, sizeof(int));
-	if (!this_visit)
-		return (free(new_que), NULL);
-	this_dist = calloc(graph->nb_vertices, sizeof(int));
-	if (!this_dist)
-		return (free(this_visit), free(new_que), NULL);
+	currvis = calloc(graph->nb_vertices, sizeof(int));
+	if (!currvis)
+		return (free(Nque), NULL);
+	currdist = calloc(graph->nb_vertices, sizeof(int));
+	if (!currdist)
+		return (free(currvis), free(Nque), NULL);
 	prev = calloc(graph->nb_vertices, sizeof(vertex_t *));
 	if (!prev)
-		return (free(this_dist), free(this_visit), free(new_que), NULL);
+		return (free(currdist), free(currvis), free(Nque), NULL);
 	/* Initialize distances to infinity, except start vertex */
 	for (i = 0; i < graph->nb_vertices; i++)
-		this_dist[i] = INT_MAX;
+		currdist[i] = INT_MAX;
 	/* Call find_dist to calculate shortest paths */
-	if (!find_dist(start, target, graph, this_visit, this_dist, prev))
+	if (!find_dist(start, target, graph, currvis, currdist, prev))
 	{
-		free(prev), free(this_dist), free(this_visit), free(new_que);
-		this_visit = NULL, this_dist = NULL, prev = NULL, new_que = NULL;
+		free(prev), free(currdist), free(currvis), free(Nque);
+		currvis = NULL, currdist = NULL, prev = NULL, Nque = NULL;
 		return (NULL);
 	}
 	/* Reconstruct path from target to start using prev array */
 	for (vert = prev[target->index]; prev[vert->index]; vert = prev[vert->index])
-		queue_push_front(new_que, strdup(vert->content));
+		queue_push_front(Nque, strdup(vert->content));
 	/* Push start and target vertices into queue */
-	queue_push_front(new_que, strdup(start->content));
-	queue_push_back(new_que, strdup(target->content));
+	queue_push_front(Nque, strdup(start->content));
+	queue_push_back(Nque, strdup(target->content));
 	/* Free dynamically allocated memory */
-	free(prev), free(this_dist), free(this_visit);
-	this_visit = NULL, this_dist = NULL, prev = NULL;
-	return (new_que);
+	free(prev), free(currdist), free(currvis);
+	currvis = NULL, currdist = NULL, prev = NULL;
+	return (Nque);
 }
 
 /**
@@ -57,43 +57,43 @@ queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
 * @start: starting node
 * @target: Target vertex
 * @graph: Graph to path
-* @this_visit: array to track if a node has been visited
-* @this_dist: array to track distance from Start
+* @currvis: array to track if a node has been visited
+* @currdist: array to track distance from Start
 * @prev: array of pointer to track path
 * Return: 1 on Success, 0 on fail.
 */
 int find_dist(vertex_t const *start, vertex_t const *target, graph_t *graph,
-			int *this_visit, int *this_dist, vertex_t **prev)
+			int *currvis, int *currdist, vertex_t **prev)
 {
-	int min_dist = INT_MAX;
+	int mindist = INT_MAX;
 	vertex_t *vert = NULL, *node = NULL;
 	edge_t *this_edge = NULL;
 
-	this_dist[start->index] = 0;
+	currdist[start->index] = 0;
 	/* Main loop to calculate shortest path distances */
 	while (node != target)
 	{
-		min_dist = INT_MAX, node = NULL;
+		mindist = INT_MAX, node = NULL;
 		/* Find unvisited vertex with smallest distance */
 		for (vert = graph->vertices; vert; vert = vert->next)
 		{
-			if (!this_visit[vert->index] && this_dist[vert->index] < min_dist)
-				min_dist = this_dist[vert->index], node = vert;
+			if (!currvis[vert->index] && currdist[vert->index] < mindist)
+				mindist = currdist[vert->index], node = vert;
 		}
 		if (!node)
 			break;
-		this_visit[node->index] = 1;
+		currvis[node->index] = 1;
 		/* Debug: print current vertex being checked and its distance */
 		printf("Checking %s, distance from %s is %d\n", node->content,
-			start->content, this_dist[node->index]);
+			start->content, currdist[node->index]);
 		/* Relax edges connected to current vertex */
 		for (this_edge = node->edges; this_edge; this_edge = this_edge->next)
 		{
-			if (!this_visit[this_edge->dest->index] &&
-				this_dist[this_edge->dest->index] >
-				min_dist + this_edge->weight)
+			if (!currvis[this_edge->dest->index] &&
+				currdist[this_edge->dest->index] >
+				mindist + this_edge->weight)
 			{
-				this_dist[this_edge->dest->index] = min_dist + this_edge->weight;
+				currdist[this_edge->dest->index] = mindist + this_edge->weight;
 				prev[this_edge->dest->index] = node;
 			}
 		}
