e52b3eed6126909b164b78f13ff8e4f4b04b50b3
matternstsr <matternstsr@gmail.com>
2024-12-09 10:07:18 -0600

    forgot to add to h file after move

--- nary_trees/nary_trees.h
+++ nary_trees/nary_trees.h
@@ -23,5 +23,5 @@ typedef struct nary_tree_s
 
 /* Function prototype */
 nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str);
-
+void nary_tree_delete(nary_tree_t *tree);
 #endif /* NARY_TREES_H */
