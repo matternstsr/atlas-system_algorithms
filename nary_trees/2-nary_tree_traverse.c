80c7dea2a56199b4f94d2796bba18b601d1befe1
matternstsr <matternstsr@gmail.com>
2024-12-10 17:07:08 -0600

    betty

--- nary_trees/2-nary_tree_traverse.c
+++ nary_trees/2-nary_tree_traverse.c
@@ -11,20 +11,21 @@ size_t nary_tree_traverse(nary_tree_t const *root,
 							void (*action)(nary_tree_t const *node,
 							size_t depth))
 {
-    static size_t depth = 0;  /* Static var to track depth w/ recursive calls */
+	static size_t depth;  /* Static var to track depth w/ recursive calls */
 
-    if (!root || !action)
-        return (0);
-    /* Perform action on current node */
-    action(root, depth);
-    /* Traverse children */
-    nary_tree_t const *child = root->children;
-    while (child)
-    {
-        depth++;  /* Increase depth for child nodes */
-        nary_tree_traverse(child, action);  /* Recursively traverse child */
-        depth--;  /* Decrease depth when returning to parent */
-        child = child->next;
-    }
-    return (depth);  /* Return maximum depth encountered */
+	if (!root || !action)
+		return (0);
+	/* Perform action on current node */
+	action(root, depth);
+	/* Traverse children */
+	nary_tree_t const *child = root->children;
+
+	while (child)
+	{
+		depth++;  /* Increase depth for child nodes */
+		nary_tree_traverse(child, action);  /* Recursively traverse child */
+		depth--;  /* Decrease depth when returning to parent */
+		child = child->next;
+	}
+	return (depth);  /* Return maximum depth encountered */
 }
